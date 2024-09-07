2b65d33d301077e4ea44e172ac2b6ac0d2fd5f67
taherehDarvishNia <darvishniatahereh@gmail.com>
2024-09-07 22:00:13 +0330

    Update atar.py

--- atar.py
+++ atar.py
@@ -1,6 +1,6 @@
-n1=int(input("n : "))
-n2=n1-1
-for i in range(1,n1):
-    print(" "*(n1-i),"*"*(2*i-(1)),sep="")
-for i in range(1,n2):
-    print(" "*(1+i),"*"*((n2)-i),"*"*((n2-1)-i),sep="") 
+number_1 = int ( input ( "number : " ) )
+number_2 = number_1 - 1
+for i in range ( 1 , number_1 ) :
+    print( " " * ( number_1 - i ) , "*" * ( 2 * i - ( 1 ) ) , sep = "" )
+for i in range ( 1 , number_2 ) :
+    print( " " * ( 1 + i ) , "*" * ( ( number_2 ) - i ) , "*" * ( ( number_2 - 1 ) - i ) , sep = "" ) 
